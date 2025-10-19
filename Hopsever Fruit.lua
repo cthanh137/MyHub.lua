b7b1b41fd930bcb0160b9285c89225ca1874d622
cthanh137 <robloxr670@gmail.com>
2025-10-19 16:01:03 +0700

    Update Hopsever Fruit.lua

--- Hopsever Fruit.lua
+++ Hopsever Fruit.lua
@@ -4,10 +4,10 @@ local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
 -- ⚙️ Cấu hình đúng theo yêu cầu
-local MIN_PLAYER_COUNT = 8
-local MAX_PLAYER_COUNT = 15
+local MIN_PLAYER_COUNT = 5
+local MAX_PLAYER_COUNT = 18
 local PLACE_ID = game.PlaceId
-local MAX_PAGES = 222
+local MAX_PAGES = 100
 local RETRY_DELAY = 1 -- tăng delay để tránh spam request
 
 local triedServers = {} -- lưu server đã thử
